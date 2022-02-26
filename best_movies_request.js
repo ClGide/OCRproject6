8f83f1dbf305463d3258402178d56705a3e32621
ClGide <90548791+ClGide@users.noreply.github.com>
2022-02-26 15:18:41 +0100

    Update best_movies_request.js

--- best_movies_request.js
+++ best_movies_request.js
@@ -2,5 +2,4 @@ import {bestMoviesQuery} from "./helpers.js"
 import {getCategoryMovies} from "./category_movies.js"
 
 let tenResults = [];
-let sevenResultsModal = [];
-getCategoryMovies([bestMoviesQuery, "best-movies--pres", tenResults, sevenResultsModal]);
+getCategoryMovies([bestMoviesQuery, "best-movies--pres", tenResults]);
