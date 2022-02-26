ce056782b505e7a72832c7d592c47f5ff3ff0b06
ClGide <90548791+ClGide@users.noreply.github.com>
2022-02-26 15:23:09 +0100

    Update scifi_request.js

--- scifi_request.js
+++ scifi_request.js
@@ -3,6 +3,4 @@ import {getCategoryMovies} from "./category_movies.js"
 
 
 let tenResults = [];
-let sevenResultsModal = [];
-getCategoryMovies([bestSciFiMoviesQuery, "scifi--pres", tenResults, sevenResultsModal]);
-
+getCategoryMovies([bestSciFiMoviesQuery, "scifi--pres", tenResults]);
