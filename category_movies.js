79e3c575a4f27c77bda9a43a20c683390a3e877c
ClGide <90548791+ClGide@users.noreply.github.com>
2022-02-26 15:21:09 +0100

    Update category_movies.js

--- category_movies.js
+++ category_movies.js
@@ -1,91 +1,97 @@
 import {API_URL, short_URL, getHTML} from "./helpers.js"
-export {filterResponse, getCategoryMovies, showCategoryMovies}
+export {filterResponseRetrieveIds, getCategoryMovies, fetchDetailsShowMovies}
+
+ 
+async function fetchDetails (sevenIds) {
+    let results = [] 
+    for (let id of sevenIds) {
+        results.push(
+            fetch(short_URL + id)
+            .then(res => res.json())
+        )
+    }
+    return Promise.all(results)
+}
 
 
-function filterResponse (data, tenResults) {
+function filterResponseRetrieveIds (data, tenResults) {
     tenResults.push(data);
     if (tenResults.length === 2) {
         let tenResultsFlat = Array.prototype.concat(tenResults[0], tenResults[1]);
-        let sevenResultsPres = tenResultsFlat.slice(0, 7);
-        return sevenResultsPres;
-    } else {
-        return "not done yet"
+        let sevenResults = tenResultsFlat.slice(0, 7);
+        let ids = sevenResults.map(result => result['id']);
+        return ids
     }
 }
 
 
-function getCategoryMovies(queryAndCatandTwoRes) {
+function getCategoryMovies(queryAndCatAndRes) {
 
-    console.log(queryAndCatandTwoRes[0])
-    for (let i = 1; i<3; i++) {
-        fetch(API_URL + String(i) + queryAndCatandTwoRes[0])
+    for (let i = 1; i<3; i++) { 
+        let query, category, tenResults;
+        [query, category, tenResults] = queryAndCatAndRes;
+
+        fetch(API_URL + String(i) + query)
         .then(res => res.json())
         .then(data => {
-            let sevenResultsPres = filterResponse(data.results, queryAndCatandTwoRes[2]);   
-            let tenResults = queryAndCatandTwoRes[2]
+        let sevenIds = filterResponseRetrieveIds(data.results, tenResults);   
             if (tenResults.length === 2) {
-                return  [sevenResultsPres, queryAndCatandTwoRes[1], tenResults, queryAndCatandTwoRes[3]];
+                return  [sevenIds, category];
             } else {
-                return ["not done yet", "not done yet", tenResults, "not done yet"];
+                return ["not done yet", "not done yet"];
             }
         })
-        .then(getCategoryMoviesDetails)
-        .then(showCategoryMovies)
+        .then(fetchDetailsShowMovies)
     }
 }
 
+function showMoviesPres (result, category) {
+    result.forEach((element, index) => {
+        const{title, image_url, description} = element;
+        const {imageEl, titleEl, summaryEl} = getHTML(category, index+1);
 
-function getCategoryMoviesDetails(queryAndCatandTwoRes) {
+        imageEl.src = image_url;
+        titleEl.innerHTML = title;
+
+        const summary = description;
+        const summaryNode = document.createTextNode(summary);
+        summaryEl.appendChild(summaryNode);
+    })
+}
+
+function showMoviesModal (result, category) {
+    result.forEach((element, index) => {
+        const {
+            genres, description, date_published, rated,
+            imdb_score, directors, actors, duration, 
+            countries, worldwide_gross_income, long_description
+        } = element
+
+    })
     
-    let sevenResultsPres, category, tenResults, sevenResultsModal;
-    [sevenResultsPres, category, tenResults, sevenResultsModal] = queryAndCatandTwoRes; 
-    
-    if (tenResults.length === 2) {
-     sevenResultsPres.forEach(element => {
-            fetch(short_URL + element['id'])
-            .then(res => res.json())
-            .then(data => 
-                sevenResultsPres.forEach(element => {
-                    element["summary"] = data["description"]
-                })
-            )    
-            .then(element => 
-                sevenResultsModal.push(element)
-            )
-        })
-        return [sevenResultsPres, category, tenResults, queryAndCatandTwoRes[3]];
+}
+
+async function fetchDetailsShowMovies(ResAndCat) {
+    let sevenIds = ResAndCat[0]
+    let category = ResAndCat[1]
+    if (sevenIds !== "not done yet") {
+        let result = await fetchDetails(sevenIds)
+        showMoviesPres(result, category)  
+        showMoviesModal(result, category) 
     } else {
-        return ["not done yet", "not done yet", tenResults, "not done yet"];
-    }
-}
-
-function collectDescriptionsAndModalRes(data) {}
-
-
-function showCategoryMovies(queryAndCatandTwoRes) {
-
-    console.log(queryAndCatandTwoRes[3])
-
-    let sevenResultsPres, category, tenResults, sevenResultsModal;
-    [sevenResultsPres, category, tenResults, sevenResultsModal] = queryAndCatandTwoRes; 
-    
-    if (tenResults.length === 2) {
-     sevenResultsPres
-        .forEach((element, index) => {
-            console.log(element)  // contains summary as a key.
-            console.log(element["summary"])  // logs undefined. 
+        console.log("waiting for the second row of fetches")
+    } 
+    /** 
+            //const {genres, description,
+            //       date_published, rated, imdb_score,
+            //       directors, actors, duration, 
+            //       countries, worldwide_gross_income, long_description} = sevenResultsModal[index];
             
-            const {title, image_url} = element;  
-            const {imageEl, titleEl, summaryEl} = getHTML(category, index+1)
+            //const summaryNode = document.createTextNode("lorem ipsum");
+            //summaryEl.appendChild(summaryNode); //adds content to the predefined one in HTML, what we want is to REPLACE it. 
 
-            imageEl.src = image_url;
-            titleEl.innerHTML = title;
-            const summaryNode = document.createTextNode("lorem ipsum");
-            summaryEl.appendChild(summaryNode); //adds content to the predefined one in HTML, what we want is to REPLACE it. 
-        
-        })
-    } else {
-        console.log("first fetch done")
-    }
+            //console.log(duration)
+
+    */
 };
 
