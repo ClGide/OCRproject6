cd7a2bb0160e5d84fce9e1098e1023bebe1866fc
ClGide <90548791+ClGide@users.noreply.github.com>
2022-02-28 14:01:33 +0100

    Update category_movies.js

--- category_movies.js
+++ category_movies.js
@@ -1,4 +1,4 @@
-import {API_URL, short_URL, getHTML} from "./helpers.js"
+import {API_URL, short_URL, getHTML, getHTMLModalWindow} from "./helpers.js"
 export {filterResponseRetrieveIds, getCategoryMovies, fetchDetailsShowMovies}
 
  
@@ -45,7 +45,7 @@ function getCategoryMovies(queryAndCatAndRes) {
     }
 }
 
-function showMoviesPres (result, category) {
+function showMovies (result, category) {
     result.forEach((element, index) => {
         const{title, image_url, description} = element;
         const {imageEl, titleEl, summaryEl} = getHTML(category, index+1);
@@ -56,28 +56,71 @@ function showMoviesPres (result, category) {
         const summary = description;
         const summaryNode = document.createTextNode(summary);
         summaryEl.appendChild(summaryNode);
-    })
-}
 
-function showMoviesModal (result, category) {
-    result.forEach((element, index) => {
         const {
-            genres, description, date_published, rated,
+            genres, date_published, rated,
             imdb_score, directors, actors, duration, 
             countries, worldwide_gross_income, long_description
         } = element
 
-    })
+        const {modalImg,
+            modalTitle,
+            modalGenre,
+            modalDate,
+            modalIMDB,
+            modalMPAA,
+            modalDirectors,
+            modalActors,
+            modalDuration,
+            modalCountries,
+            modalBoxOffice,
+            modalDescription} = getHTMLModalWindow(category, index+1);
+
+        modalImg.src = image_url
+
+        const titleContent = document.createTextNode(title)
+        modalTitle.appendChild(titleContent)
+        
+        const genreContent = document.createTextNode(genres)
+        modalGenre.appendChild(genreContent)
     
+        const dateContent = document.createTextNode(date_published)
+        modalDate.appendChild(dateContent)
+         
+        const IMDBContent = document.createTextNode(imdb_score)
+        modalIMDB.appendChild(IMDBContent)
+    
+        const contentMPAA = document.createTextNode(rated)
+        modalMPAA.appendChild(contentMPAA)
+    
+        const contentDirectors = document.createTextNode(directors)
+        modalDirectors.appendChild(contentDirectors)
+    
+        const contentActors = document.createTextNode(actors)
+        modalActors.appendChild(contentActors)
+    
+        const contentDuration = document.createTextNode(duration)
+        modalDuration.appendChild(contentDuration)
+    
+        const contentCountries = document.createTextNode(countries)
+        modalCountries.appendChild(contentCountries)
+    
+        const contentBoxOffice = document.createTextNode(worldwide_gross_income)
+        modalBoxOffice.append(contentBoxOffice)
+    
+        const descriptionContent = document.createTextNode(long_description)
+        modalDescription.appendChild(descriptionContent)
+    
+    })
 }
+
 
 async function fetchDetailsShowMovies(ResAndCat) {
     let sevenIds = ResAndCat[0]
     let category = ResAndCat[1]
     if (sevenIds !== "not done yet") {
         let result = await fetchDetails(sevenIds)
-        showMoviesPres(result, category)  
-        showMoviesModal(result, category) 
+        showMovies(result, category)  
     } else {
         console.log("waiting for the second row of fetches")
     } 
