c0b71f5453dfdc1562fcd2b75076eb8ae112a52f
ClGide <90548791+ClGide@users.noreply.github.com>
2022-03-19 11:59:54 +0100

    Update control_modal.js

--- control_modal.js
+++ control_modal.js
@@ -3,26 +3,16 @@ const BtnOpenImg = document.getElementsByClassName("btn-open-img");
 const modal = document.getElementsByClassName("modal");
 const BtnClosed = document.getElementsByClassName("btn-closed");
 
+for (let i = 0; i < 29; i++) {
+  BtnOpen[i].addEventListener("click", function () {
+    modal[i].classList.add("show");
+  });
 
-for (let i=0; i<29; i++) {
+  BtnOpenImg[i].addEventListener("click", function () {
+    modal[i].classList.add("show");
+  });
 
-    BtnOpen[i].addEventListener(
-        "click", function (){
-            modal[i].classList.add("show");
-    
-        }
-    );
-
-    BtnOpenImg[i].addEventListener(
-        "click", function (){
-            modal[i].classList.add("show");
-    
-        }
-    );
-    
-    BtnClosed[i].addEventListener(
-        "click", function() {
-            modal[i].classList.remove("show")
-       }
-    );
+  BtnClosed[i].addEventListener("click", function () {
+    modal[i].classList.remove("show");
+  });
 }
